6329fe6da7604054a1c4d27fd8de9eb8a3f470d7
Syed Hisham Akmal <hisham0502@gmail.com>
2023-08-13 19:18:39 +0530

    Replacing structOpt with clap as structOpt is redundant

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::fs;
-use structopt::StructOpt; //StructOpt trait->structopt crate, used parsing command-line arguments.
+// use structopt::StructOpt; //StructOpt trait->structopt crate, used parsing command-line arguments.
+use clap::Parser; //using Clap as an alternative for structOpt
 use std::path::PathBuf; //PathBuf type -> std::path module, which is used to manipulate paths.
 
-#[derive(StructOpt)] // deriving StructOpt trait(implements the StructOpt trait for the Cli struct) - https://doc.rust-lang.org/rust-by-example/trait/derive.html
+#[derive(Parser,Default,Debug)]
+// #[derive(StructOpt)] // deriving StructOpt trait(implements the StructOpt trait for the Cli struct) - https://doc.rust-lang.org/rust-by-example/trait/derive.html
 struct Cli {
-    #[structopt(parse(from_os_str))]
+    // #[structopt(parse(from_os_str))]
     path: PathBuf, //path field is  parsed from a command-line argument and converted into a PathBuf
 }
 
 fn main() {
-    let args = Cli::from_args();
+    let args = Cli::parse();
     println!("Path: {:?}", args.path);
 }
 
