56311e2256a5102c390c99e4d811b7c658e43599
Syed Hisham Akmal <hisham0502@gmail.com>
2023-08-13 19:27:34 +0530

    Basic clap setup in place

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 use clap::Parser; //using Clap as an alternative for structOpt
 use std::path::PathBuf; //PathBuf type -> std::path module, which is used to manipulate paths.
 
-#[derive(Parser,Default,Debug)]
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
 // #[derive(StructOpt)] // deriving StructOpt trait(implements the StructOpt trait for the Cli struct) - https://doc.rust-lang.org/rust-by-example/trait/derive.html
 struct Cli {
     // #[structopt(parse(from_os_str))]
