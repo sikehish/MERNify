6bb0fd26ec33e0dc7f188bfb574b23373bf26ec7
Syed Hisham Akmal <hisham0502@gmail.com>
2023-08-21 23:55:31 +0530

    Rewriting the app

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
+use clap::{App, Arg};
 use std::fs;
-// use structopt::StructOpt; //StructOpt trait->structopt crate, used parsing command-line arguments.
-use clap::Parser; //using Clap as an alternative for structOpt
-use std::path::PathBuf; //PathBuf type -> std::path module, which is used to manipulate paths.
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-// #[derive(StructOpt)] // deriving StructOpt trait(implements the StructOpt trait for the Cli struct) - https://doc.rust-lang.org/rust-by-example/trait/derive.html
-struct Cli {
-    // #[structopt(parse(from_os_str))]
-    path: PathBuf, //path field is  parsed from a command-line argument and converted into a PathBuf
-}
-
-fn main() {
-    let args = Cli::parse();
-    println!("Path: {:?}", args.path);
-}
-
-//Run command: cargo run -- pathname
+use std::path::Path;
+use std::process::Command;
 
